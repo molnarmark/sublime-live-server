9a075399f3a6dbd5fe0583ad356c4f6d11778ed6
Mark Molnar <molnarpw@gmail.com>
2020-11-28 03:03:24 +0100

    Initial commit

---
+++ plugin.py
@@ -0,0 +1,11 @@
+import sublime
+import sublime_plugin
+
+class LiveServerStartCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    pass
+
+
+class LiveServerStopCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    pass
